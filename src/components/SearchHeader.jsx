3d8adabb2118971223a9bbfc100336a38d4e5801
Divyverma <divyanshverma689@gmail.com>
2024-03-06 21:53:01 +0530

    search header option completed

--- src/components/SearchHeader.jsx
+++ src/components/SearchHeader.jsx
@@ -1,8 +1,9 @@
 import Link from "next/link";
 import Image from "next/image";
 import SearchBox from "./SearchBox";
-// import { RiSettings3Line } from 'react-icons/ri';
+import { RiSettings3Line } from "react-icons/ri";
 import { TbGridDots } from "react-icons/tb";
+import SearchHeaderOptions from "./SearchHeaderOptions";
 
 
 export default function SearchHeader() {
@@ -24,12 +25,13 @@ export default function SearchHeader() {
                 <SearchBox/>
             </div>
 
-            <div className="">
-                {/* <RiSettings3Line/> */}
-                <TbGridDots/>
+            <div className=" hidden md:inline-flex space-x-2">
+                <RiSettings3Line className="bg-transparent hover:bg-gray-200 p-2 text-4xl rounded-full cursor-pointer" />
+                <TbGridDots className="bg-transparent hover:bg-gray-200 p-2 text-4xl rounded-full cursor-pointer" />
             </div>
-            <button>Sign in</button>
+            <button className="bg-blue-500 text-white px-6 py-2 rounded-md hover:brightness-105 hover:shadow-md transition-shadow ml-2" >Sign in</button>
         </div>
+        <SearchHeaderOptions/>
     </header>
   )
 }
